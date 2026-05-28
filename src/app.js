8fac35125ad9ab187e8c3b19fe6ad2a69792fe50
VittalPrasad <darpalvittalprasad@gmail.com>
2026-05-28 21:28:44 +0530

    added validations

--- src/app.js
+++ src/app.js
@@ -16,6 +16,39 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("Something went wrong!!!");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete(userId);
+    res.send("user deleted successfully!!!!");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    res.send("user updated successfully!!!!");
+  } catch (err) {
+    res.status(400).send("update failed:" + err.message);
+  }
+});
+
 connectDB()
   .then(() => {
     console.log("database connection established......");
--- src/models/user.js
+++ src/models/user.js
@@ -1,25 +1,62 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 4,
+      maxLength: 50,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      minLength: 4,
+      maxLength: 50,
+      trim: true,
+    },
+    emailId: {
+      type: String,
+      lowerCase: true,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+      trim: true,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new error("Gender data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://www.pngitem.com/pimgs/m/581-5813504_avatar-dummy-png-transparent-png.png",
+    },
+    about: {
+      type: String,
+      default: "THis is default description of the user!!",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
+  {
+    timestamps: true,
   },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+);
 
 const User = mongoose.model("User", userSchema);
 
